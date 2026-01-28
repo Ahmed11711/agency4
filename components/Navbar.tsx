ea9617961517d520040329c48ed3b4a251c43a9c
Ahmed11711 <ahmed11711@gmail.com>
2026-01-28 19:51:54 +0200

    sss

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -64,7 +64,6 @@ const Navbar: React.FC = () => {
             <span className="text-xl font-bold tracking-tight text-white dark:text-white light:text-gray-900 uppercase leading-none">
               Professional Advertising
             </span>
-            <span className="text-[10px] tracking-[0.2em] text-gray-400 dark:text-gray-400 light:text-gray-600 uppercase font-medium"></span>
           </div>
         </Link>
 
